8d4e6f99eec1380cda01f1abcc56e4a0a9156baa
Lucas Velásquez <lucas.vel@outlook.com>
2022-02-08 21:32:43 -0300

    navbar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () =>{
+    return(
+        <nav>
+            <a>Inico</a>
+            <a>Catalogo</a>
+            <a>Otras tiendas</a>
+        </nav>
+    )
+}
+
+export default NavBar;
